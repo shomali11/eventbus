2173550715339639279edc8302a88b3216faf7f4
Raed Shomali <shomali11@gmail.com>
2017-08-06 09:54:01 -0400

    Add tests

--- eventbus_test.go
+++ eventbus_test.go
@@ -35,3 +35,22 @@ func TestEventbus_Success(t *testing.T) {
 
 	time.Sleep(time.Second)
 }
+
+func TestEventbus_Multi(t *testing.T) {
+	client := NewClient()
+	defer client.Close()
+
+	client.Subscribe("name", func(value interface{}) {
+		assert.Equal(t, value, "Raed Shomali")
+	})
+
+	client.Subscribe("name", func(value interface{}) {
+		assert.Equal(t, value, "Raed Shomali")
+	})
+
+	time.Sleep(time.Second)
+
+	client.Publish("name", "Raed Shomali")
+
+	time.Sleep(time.Second)
+}
