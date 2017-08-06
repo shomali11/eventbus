c43969d2dccf75e12b8434b8dde1ef6d6373f796
Raed Shomali <shomali11@gmail.com>
2017-08-06 09:51:25 -0400

    Add tests

---
+++ eventbus_test.go
@@ -0,0 +1,37 @@
+package eventbus
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+func TestEventbus_Fail(t *testing.T) {
+	client := NewClient()
+	defer client.Close()
+
+	client.Publish("unknown", "Unknown")
+
+	time.Sleep(time.Second)
+
+	client.Subscribe("unknown", func(value interface{}) {
+		assert.Fail(t, "Not supposed to be reached")
+	})
+
+	time.Sleep(time.Second)
+}
+
+func TestEventbus_Success(t *testing.T) {
+	client := NewClient()
+	defer client.Close()
+
+	client.Subscribe("name", func(value interface{}) {
+		assert.Equal(t, value, "Raed Shomali")
+	})
+
+	time.Sleep(time.Second)
+
+	client.Publish("name", "Raed Shomali")
+
+	time.Sleep(time.Second)
+}
